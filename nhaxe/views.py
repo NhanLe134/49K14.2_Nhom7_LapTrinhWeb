6f173a42b75327c71b10288cc31e943fce373162
Huân <nvg.huandn@gmail.com>
2026-04-14 23:06:21 +0700

    Adjustment: Automate API sync in views and restore original UI (removed manual sync button)

--- nhaxe/views.py
+++ nhaxe/views.py
@@ -125,36 +125,25 @@ def thong_tin_nha_xe(request):
 
             return JsonResponse({'status': 'success', 'message': 'Cập nhật thông tin thành công!'})
 
-def api_sync(request):
-    from .sync_manager import SyncManager
-    from django.contrib import messages
-    
-    model_type = request.GET.get('type', 'all')
-    token = request.session.get('token')
-    sync_mgr = SyncManager(token=token)
-    
-    if model_type == 'loaixe':
-        count, msg = sync_mgr.sync_loaixe()
-    elif model_type == 'xe':
-        count, msg = sync_mgr.sync_xe()
-    else:
-        count, msg = sync_mgr.sync_all()
-        
-    messages.success(request, msg)
-    
-    referrer = request.META.get('HTTP_REFERER', 'nhaxe')
-    return redirect(referrer)
-
 def quanlytuyenxe(request):
     return render(request, 'home/quanlytuyenxe.html')
 
 def quanly_loaixe(request):
+    from .sync_manager import SyncManager
+    # Automatic sync when visiting the page
+    sync_mgr = SyncManager(token=request.session.get('token'))
+    sync_mgr.sync_loaixe()
     return render(request, 'home/quanly_loaixe.html')
 
 def quan_ly_xe(request):
+    from .sync_manager import SyncManager
     nha_xe_id = request.session.get('user_id')
     if not nha_xe_id:
         return redirect('dangnhap')
+    
+    # Automatic sync of vehicles and types when visiting the page
+    sync_mgr = SyncManager(token=request.session.get('token'))
+    sync_mgr.sync_all()
     
     # Check if Nhaxe exists locally, create if missing (since we auth via external API)
     nha_xe = Nhaxe.objects.filter(NhaxeID=nha_xe_id).first()
